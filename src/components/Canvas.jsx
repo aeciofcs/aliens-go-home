37a20c12308a148eae03bb3c2e7fa6ea921a744d
Aecio Silva <aeciofcs@gmail.com>
2020-05-25 15:00:48 -0300

    Create Component Canvas

---
+++ src/components/Canvas.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Canvas = () => {
+    
+    //const style = { border: '1px solid black' };
+    const viewBox = [window.innerWidth / -2, 
+                     100 - window.innerHeight, 
+                     window.innerWidth, 
+                     window.innerHeight];
+
+    return (
+        <svg 
+            id="aliens-go-home-canvas"
+            preserveAspectRatio="xMaxYMax nome"
+            //style={style}
+            viewBox={viewBox}
+        >
+            <circle cx={0} cy={0} r={50} />
+        </svg>
+    );
+};
+
+export default Canvas;
