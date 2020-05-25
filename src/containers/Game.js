12c78858207a59ca40af052dd09fe9291c0e024f
Aecio Silva <aeciofcs@gmail.com>
2020-05-25 17:16:08 -0300

    Enabling Users to Start the Game

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -1,15 +1,21 @@
 import { connect } from 'react-redux';
 import App from '../App'
-import { moveObjects } from '../actions/index';
+import { moveObjects, startGame } from '../actions/index';
 
 const mapStateToProps = state => ({
     //message: state.message,
     angle: state.angle,
+    gameState: state.gameState,
 });
 
 const mapDispatchToProps = dispatch => ({
+    
     moveObjects: (mousePosition) => {
         dispatch(moveObjects(mousePosition));
+    },
+
+    startGame: () => {
+        dispatch(startGame());
     },
 });
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,17 +1,25 @@
-import { MOVE_OBJECTS } from '../actions/index'
+import { MOVE_OBJECTS, START_GAME } from '../actions/index'
 import moveObjects from './moveObjects'
+import startGame from './startGame';
 
+const initialGameState = {
+    started: false,
+    kills: 0,
+    lives: 3,
+};
 
 const initalState = {
     //message: `It's easy to integrate React and Redux, isn't it?`,
     angule: 45,
+    gameState: initialGameState,
 };
 
 function reducer(state = initalState, action){
     switch (action.type) {
         case MOVE_OBJECTS:
             return moveObjects(state, action);
-
+        case START_GAME:
+            return startGame(state, initialGameState);
         default:
             return state;
     }
