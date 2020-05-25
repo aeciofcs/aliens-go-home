4c560075aa40be930623b4ea71fdbc4e2bf2e7fb
Aecio Silva <aeciofcs@gmail.com>
2020-05-25 16:26:50 -0300

    Making the Cannon Aim

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,21 @@
+import { MOVE_OBJECTS } from '../actions/index'
+import moveObjects from './moveObjects'
+
+
 const initalState = {
-    message: `It's easy to integrate React and Redux, isn't it?`,
+    //message: `It's easy to integrate React and Redux, isn't it?`,
+    angule: 45,
 };
 
-function reducer(state = initalState){
-    return state;
+function reducer(state = initalState, action){
+    switch (action.type) {
+        case MOVE_OBJECTS:
+            return moveObjects(state, action);
+
+        default:
+            return state;
+    }
+    
 }
 
 export default reducer;
