000153b43340092bf7ced57055abc9ddc5ba9be7
Aecio Silva <aeciofcs@gmail.com>
2020-05-25 16:56:08 -0300

    create component title

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -9,6 +9,7 @@ import CurrentScore from './CurrentScore';
 import FlyingObject from './FlyingObject';
 import Heart from './Heart';
 import StartGame from './StartGame';
+import Title from './Title';
 
 const Canvas = (props) => {
     const viewBox = [window.innerWidth / -2, 
@@ -38,6 +39,7 @@ const Canvas = (props) => {
             <FlyingObject position={{ x: 150, y: -300 }} />
             <Heart position={{x: -300, y: 35}} />
             <StartGame onClick={() => console.log('Aliens, Go Home!')} />
+            <Title />
         </svg>
     );
 };
