66938b0b9499704d802e469ad76ec9ef7cf35484
Aecio Silva <aeciofcs@gmail.com>
2020-05-26 09:49:55 -0300

    Using CSS Animation to Move Flying Objects

--- src/reducers/moveObjects.js
+++ src/reducers/moveObjects.js
@@ -2,6 +2,7 @@ import { calculateAngle } from '../utils/formulas';
 import createFlyingObjects from './createFlyingObjects';
 
 function moveObjects(state, action) {
+
     const mousePosition = action.mousePosition || {
         x: 0,
         y: 0,
@@ -9,10 +10,22 @@ function moveObjects(state, action) {
     
     const newState = createFlyingObjects(state);
 
+    const now = (new Date()).getTime();
+    const flyingObjects = newState.gameState.flyingObjects.filter(object => (
+        (now - object.createdAt) < 4000
+    ));
+
     const { x, y } = mousePosition;
+
     const angle = calculateAngle(0, 0, x, y);
+
+
     return {
         ...newState,
+        gameState: {
+            ...newState.gameState,
+            flyingObjects,
+        },
         angle,
     };
 }
