ee8c1b749988ed271a62aa5ce4b1ea9ce5e29867
Aecio Silva <aeciofcs@gmail.com>
2020-05-26 11:07:07 -0300

    changing twitter photo link in Canvas

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -22,14 +22,14 @@ const Canvas = (props) => {
                      gameHeight];
     
     const leaderboard = [
-        { id: 'd4', maxScore: 82, name: 'Ado Kukic', picture: 'https://twitter.com/KukicAdo/photo', },
-        { id: 'a1', maxScore: 235, name: 'Bruno Krebs', picture: 'https://twitter.com/brunoskrebs/photo', },                                                                  
-        { id: 'c3', maxScore: 99, name: 'Diego Poza', picture: 'https://twitter.com/diegopoza/photo', },
-        { id: 'b2', maxScore: 129, name: 'Jeana Tahnk', picture: 'https://twitter.com/jeanatahnk/photo', },
-        { id: 'e5', maxScore: 34, name: 'Jenny Obrien', picture: 'https://twitter.com/jenny_obrien/photo', },
-        { id: 'f6', maxScore: 153, name: 'Kim Maida', picture: 'https://twitter.com/KimMaida/photo', },
-        { id: 'g7', maxScore: 55, name: 'Luke Oliff', picture: 'https://twitter.com/mroliff/photo', },
-        { id: 'h8', maxScore: 146, name: 'Sebastian Peyrott', picture: 'https://twitter.com/speyrott/photo', },
+        { id: 'd4', maxScore: 82, name: 'Ado Kukic', picture: 'https://pbs.twimg.com/profile_images/1228501915991863296/29iVDHX5_400x400.jpg', },
+        { id: 'a1', maxScore: 235, name: 'Bruno Krebs', picture: 'https://pbs.twimg.com/profile_images/1243511797610688512/Ubtxm_Q3_400x400.jpg', },                                                                  
+        { id: 'c3', maxScore: 99, name: 'Diego Poza', picture: 'https://pbs.twimg.com/profile_images/478612888866009088/ySRi3jxT_400x400.jpeg', },
+        { id: 'b2', maxScore: 129, name: 'Jeana Tahnk', picture: 'https://pbs.twimg.com/profile_images/1891692507/JeanaTahnk2_crop_400x400.jpg', },
+        { id: 'e5', maxScore: 34, name: 'Jenny Obrien', picture: 'https://pbs.twimg.com/profile_images/558163656500715520/88sTtRBT_400x400.png', },
+        { id: 'f6', maxScore: 153, name: 'Kim Maida', picture: 'https://pbs.twimg.com/profile_images/1202690224440168465/-uOewZUp_400x400.jpg', },
+        { id: 'g7', maxScore: 55, name: 'Luke Oliff', picture: 'https://abs.twimg.com/sticky/default_profile_images/default_profile_400x400.png', },
+        { id: 'h8', maxScore: 146, name: 'Sebastian Peyrott', picture: 'https://pbs.twimg.com/profile_images/631528500042825729/9giF9-bh_400x400.png', },
     ];
 
     return (
