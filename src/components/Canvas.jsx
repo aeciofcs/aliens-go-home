9686ec31f2cdea2d0f79d12e38f7c82cb5efb764
Aecio Silva <aeciofcs@gmail.com>
2020-05-26 10:59:55 -0300

    Creating the Leaderboard React Component

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -12,6 +12,7 @@ import StartGame from './StartGame';
 import Title from './Title';
 import Login from './Login';
 import { signIn } from 'auth0-web';
+import Leaderboard from './Leaderboard';
 
 const Canvas = (props) => {
     const gameHeight = 1200;
@@ -19,6 +20,17 @@ const Canvas = (props) => {
                      100 - gameHeight, 
                      window.innerWidth, 
                      gameHeight];
+    
+    const leaderboard = [
+        { id: 'd4', maxScore: 82, name: 'Ado Kukic', picture: 'https://twitter.com/KukicAdo/photo', },
+        { id: 'a1', maxScore: 235, name: 'Bruno Krebs', picture: 'https://twitter.com/brunoskrebs/photo', },                                                                  
+        { id: 'c3', maxScore: 99, name: 'Diego Poza', picture: 'https://twitter.com/diegopoza/photo', },
+        { id: 'b2', maxScore: 129, name: 'Jeana Tahnk', picture: 'https://twitter.com/jeanatahnk/photo', },
+        { id: 'e5', maxScore: 34, name: 'Jenny Obrien', picture: 'https://twitter.com/jenny_obrien/photo', },
+        { id: 'f6', maxScore: 153, name: 'Kim Maida', picture: 'https://twitter.com/KimMaida/photo', },
+        { id: 'g7', maxScore: 55, name: 'Luke Oliff', picture: 'https://twitter.com/mroliff/photo', },
+        { id: 'h8', maxScore: 146, name: 'Sebastian Peyrott', picture: 'https://twitter.com/speyrott/photo', },
+    ];
 
     return (
         <svg 
@@ -43,7 +55,7 @@ const Canvas = (props) => {
                 <g>
                     <StartGame onClick={() => props.startGame() } />
                     <Title />
-                    <Login authenticate={signIn} />
+                    <Leaderboard currentPlayer={leaderboard[6]} authenticate={signIn} leaderboard={leaderboard} />                    
                 </g>
             }
             {props.gameState.started &&
